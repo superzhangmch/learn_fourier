b8e3e61300c76ed7b9063fe050784a0ccb87920d
Zhang Miaochang <superzhangmch@163.com>
2024-11-20 12:30:33 +0800

    Update just-fourier-things.js

--- js/just-fourier-things.js
+++ js/just-fourier-things.js
@@ -4,7 +4,7 @@ import { slurp } from './util.js';
 /**
  * Do the fourier thing using a bunch of complex points
  *
- * @param {Array<Number>} points Array of points, alternative with re, im pairs. Length must be a power of 2
+ * @param {Array<Number>} points Array of points, alternative with re, im pairs【所以re, im 就是{x,y}坐标点了】. Length must be a power of 2
  */
 export function getFourierData(points) {
     if (points.length == 0) {
@@ -15,6 +15,20 @@ export function getFourierData(points) {
 
     const out = fft.createComplexArray();
     fft.transform(out, points);
+    /*
+    快速傅里叶变换（FFT）的输入和输出主要涉及到复杂数的序列。具体来说：
+    输入:
+    时域信号：通常是一个实数或复数序列。这个序列代表的是信号在时域上的采样值。
+    长度：FFT 的输入长度通常是 2 的幂次方（例如 256、512、1024 等），因为这样可以利用 FFT 算法的效率。对于非 2 的幂次长度的信号，可以通过零填充（zero-padding）来扩展信号长度。
+    输出:
+    频域信号：输出是一个复数序列，每个复数代表一个频率分量的幅度和相位。
+    
+    幅度（Magnitude）：通过计算复数的模，可以得到每个频率分量的幅度。
+    相位（Phase）：通过计算复数的辐角，可以得到每个频率分量的相位。
+    频率分辨率：输出序列的长度与输入相同。对于实数输入，输出通常是对称的，其中前半部分代表正频率，后半部分代表负频率（在某些实现中，负频率部分可能会被省略）。
+    
+    FFT 的主要用途是将时域信号转换为频域信号，使得信号分析、滤波、频谱分析等变得更加方便和直观。
+    */
 
     // Transform into an API of points I find friendlier.
     const fftData = [];
@@ -25,9 +39,9 @@ export function getFourierData(points) {
         const y = out[2 * j + 1];
         const freq = ((j + numPoints / 2) % numPoints) - numPoints / 2;
         fftData.push({
-            freq: freq,
+            freq: freq,                                      // FFT 返回的数组的每一个元素对应一定频率
             // a little expensive
-            amplitude: Math.sqrt(x * x + y * y) / numPoints,
+            amplitude: Math.sqrt(x * x + y * y) / numPoints, // FFT 返回的数组的每一个元素的取值，决定了一定频率下的振幅与相位(phase)
             // a lottle expensive :(
             phase: Math.atan2(y, x),
         });
@@ -77,6 +91,9 @@ export function getRealFourierData(points) {
  * Transforms a list of x, y points into input appropriate for a fourier transform.
  */
 export function resample2dData(points, numSamples) {
+    /*
+    不管原来有多少个{x, y} 点，把 points 整成 numSamples 个 {x, y} 点。为保持弄完后首位相连画出的图要和处理前画出的尽量一样，新点要在旧点基础上插值拿到
+    */
     if (points.length == 0) {
         // Can't resample if we don't have ANY points
         return [];
@@ -88,7 +105,7 @@ export function resample2dData(points, numSamples) {
         let nextIndex = (index + 1) % points.length;
         let amt = position - index;
         newPoints.push(
-            /* x */ slurp(points[index].x, points[nextIndex].x, amt),
+            /* x */ slurp(points[index].x, points[nextIndex].x, amt), // slurp: function slurp(val1, val2, amt) {return (val2 - val1) * amt + val1;}, 即作插值拿到新点位置
             /* y */ slurp(points[index].y, points[nextIndex].y, amt),
         )
     }
